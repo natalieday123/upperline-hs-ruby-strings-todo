f7f4b5e5849c3a42c6ba0e5c14e5d848f8ec7198
Natalie <nday2018@ihs.immaculateheart.org>
2017-08-30 08:57:04 -0700

    changes to variable practice

--- invitation.rb
+++ invitation.rb
@@ -2,8 +2,10 @@ puts "What is the guest's name?"
 persons_name = gets.chomp
 puts "What is your party name?"
 party_name = gets.chomp
-puts "What is the date of your party?"
-party_date = gets.chomp
+puts "What is the month of your party?"
+party_month = gets.chomp
+puts "What is the day of your party?"
+party_day = gets.chomp
 puts "What is the time of your party?"
 party_time = gets.chomp
 puts "What is your name?"
@@ -11,5 +13,7 @@ host_name = gets.chomp
 
 puts "Dear #{persons_name},
 You are cordially invited to #{party_name} on
-#{party_date} at #{party_time}..Sincerely, #{host_name}
+#{party_month} #{party_day} at #{party_time}. Please
+RSVP by #{party_month} #{party_day.to_i - 1}
+.Sincerely, #{host_name}
 "
